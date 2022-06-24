ef2384591225ebd7d8b7a83cdd03d5ba64d5cd4b
Esteban Cespedes <mmonestelalf@gmail.com>
2022-06-24 11:50:07 -0600

    se agregaron nuevos ejercicios

---
+++ figuras.js
@@ -0,0 +1,26 @@
+console.group("Cuadrado")
+const ladoCuadrado = 5;
+console.log("Los lados del cuadrado miden" + ladoCuadrado + "cm");
+
+const perimetroCuadrado = ladoCuadrado * 4;
+console.log("El perimetro del cuadrado es:" + perimetroCuadrado + "cm");
+
+const areaCuadrado = ladoCuadrado * ladoCuadrado;
+console.log("El area del cuadrado es:" + areaCuadrado + "cm");
+console.groupEnd();
+
+
+console.group("Triangulo")
+
+const ladoTriangulo = 6;
+const ladoTriangulo2 = 5;
+const baseTriangulo = 4;
+
+console.log("Los lados del cuadrado miden"
+ + ladoTriangulo + "cm,"
+ +  ladoTriangulo2 + "cm,"
+ + baseTriangulo + "cm");
+
+ console.groupEnd();
+
+
